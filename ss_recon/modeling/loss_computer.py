n2r bug fixes w/ gradient flow

ss_recon/modeling/loss_computer.py
@@ -12,6 +12,7 @@ The registered object will be called with `obj(cfg)`
 and expected to return a LossComputer object.
 """
 
+EPS = 1e-8
 
 def build_loss_computer(cfg, name):
     return LOSS_COMPUTER_REGISTRY.get(name)(cfg)
@@ -72,7 +73,7 @@ class N2RLossComputer(object):
         """
         if output is None or len(output) == 0:
             return {
-                k: torch.Tensor([0.0]) for k in ["l1", "l2", "psnr"]
+                k: torch.Tensor([0.0]).detach() for k in ["l1", "l2", "psnr", "loss"]
             }
 
         pred: torch.Tensor = output["pred"]
@@ -89,7 +90,7 @@ class N2RLossComputer(object):
         abs_error = cplx.abs(output - target)
         l1 = torch.mean(abs_error)
         l2 = torch.sqrt(torch.mean(abs_error ** 2))
-        psnr = 20 * torch.log10(cplx.abs(target).max() / l2)
+        psnr = 20 * torch.log10(cplx.abs(target).max() / (l2 + EPS))
 
         metrics_dict = {"l1": l1, "l2": l2, "psnr": psnr}
         metrics_dict["loss"] = metrics_dict[loss]
@@ -117,21 +118,26 @@ class N2RLossComputer(object):
 
     def __call__(self, input, output):
         output_recon = output.get("recon", None)
-        output_consistency = output("consistency", None)
+        output_consistency = output.get("consistency", None)
 
+        loss = 0
         metrics_recon = {
             "recon_{}".format(k): v
-            for k, v in self._compute_metrics(output_recon, self.recon_loss)
+            for k, v in self._compute_metrics(output_recon, self.recon_loss).items()
         }
+        if output_recon is not None:
+            loss += metrics_recon["recon_loss"]
 
         metrics_consistency = {
             "cons_{}".format(k): v
-            for k, v in self._compute_metrics(output_consistency, self.consistency_loss)  # noqa
+            for k, v in self._compute_metrics(output_consistency, self.consistency_loss).items() # noqa
         }
+        if output_consistency is not None:
+            loss += self.consistency_weight * metrics_consistency["cons_loss"]
 
         metrics_consistency.update(metrics_recon)
         metrics = metrics_consistency
 
-        metrics["loss"] = metrics["recon_loss"] + self.consistency_weight * metrics["cons_loss"]  # noqa
+        metrics["loss"] = loss
         return metrics
 

ss_recon/modeling/meta_arch/n2r.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn
 
+from ss_recon.utils import complex_utils as cplx
 from .build import META_ARCH_REGISTRY
 from .unrolled import GeneralizedUnrolledCNN
 
@@ -17,7 +18,17 @@ class N2RModel(nn.Module):
         self.use_base_grad = False
 
     def augment(self, inputs):
-        """Noise augmentation module."""
+        """Noise augmentation module.
+        TODO: Perform the augmentation here.
+        """
+        kspace = inputs["kspace"].clone()
+        mask = cplx.get_mask(kspace)
+
+        # Replace line below with augmentation.
+        aug_kspace = kspace 
+
+        inputs = {k: v.clone() for k, v in inputs.items() if k != "kspace"}
+        inputs["kspace"] = aug_kspace
         return inputs
 
     def forward(self, inputs):
@@ -30,6 +41,8 @@ class N2RModel(nn.Module):
 
         inputs_supervised = inputs.get("supervised", None)
         inputs_unsupervised = inputs.get("unsupervised", None)
+        if inputs_supervised is None and inputs_unsupervised is None:
+            raise ValueError("Examples not formatted in the proper way")
         output_dict = {}
 
         # Recon
@@ -39,15 +52,14 @@ class N2RModel(nn.Module):
             )
 
         # Consistency.
-        # TODO: If this takes a long time, construct SenseModel beforehand.
         if inputs_unsupervised is not None:
             inputs_us_aug = self.augment(inputs_unsupervised)
-            with torch.set_grad_enabled(self.use_base_grad):
+            with torch.no_grad():
                 pred_base = self.unrolled(inputs_unsupervised)["pred"]
             pred_aug = self.unrolled(inputs_us_aug, return_pp=True)
             if "target" in pred_aug:
                 del pred_aug["target"]
-            pred_aug["target"] = pred_base
+            pred_aug["target"] = pred_base.detach()
             output_dict["consistency"] = pred_aug
 
         return output_dict

ss_recon/modeling/meta_arch/unrolled.py
@@ -148,7 +148,7 @@ class GeneralizedUnrolledCNN(nn.Module):
         target = inputs["target"].to(device) if "target" in inputs else None
         mask = inputs["mask"].to(device) if "mask" in inputs else None
         A = inputs["sense_model"].to(device) if "sense_model" in inputs else None
-
+        maps = inputs["maps"].to(device)
         if self.num_emaps != maps.size()[-2]:
             raise ValueError(
                 "Incorrect number of ESPIRiT maps! Re-prep data..."
@@ -166,7 +166,6 @@ class GeneralizedUnrolledCNN(nn.Module):
 
         # Declare signal model.
         if A is None:
-            maps = inputs["maps"].to(device)
             A = SenseModel(maps, weights=mask)
 
         # Compute zero-filled image reconstruction