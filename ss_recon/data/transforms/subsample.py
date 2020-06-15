3ac1e233d3d3b8bdb49d582a8c865b8ba175879f
Arjun Desai <arjundd@stanford.edu>
2020-06-15 08:03:19 -0700

    add support for poisson undersampling

--- ss_recon/data/transforms/subsample.py
+++ ss_recon/data/transforms/subsample.py
@@ -1,3 +1,4 @@
+import numba as nb
 import numpy as np
 import sigpy.mri
 import torch
@@ -105,14 +106,16 @@ class PoissonDiskMaskFunc(MaskFunc):
 
         # Generate undersampling mask
         # NOTE: Due to a sigpy bug, fixing a seed will cause a change
-        # in the fixed numpy seed. Note this can cause downstream reproducibility
-        # issues.
-        mask = sigpy.mri.poisson(
+        # in the fixed numpy seed.
+        # To avoid this issue this method has been duplicated from
+        # sigpy and added below.
+        # https://github.com/mikgroup/sigpy/issues/54
+        mask = poisson(
             (nky, nkz),
             acceleration,
             calib=self.calib_size,
             dtype=np.float32,
-            seed=np.random.seed(seed),
+            seed=seed,
         )
 
         # Reshape the mask
@@ -120,3 +123,136 @@ class PoissonDiskMaskFunc(MaskFunc):
 
         return mask
 
+
+# ================================================================ #
+# Adapted from sigpy.
+# Duplicated because of https://github.com/mikgroup/sigpy/issues/54
+# TODO: Remove once https://github.com/mikgroup/sigpy/issues/54 is
+# solved and added to release.
+# ================================================================ #
+def poisson(img_shape, accel, K=30, calib=[0, 0], dtype=np.complex,
+            crop_corner=True, return_density=False, seed=0):
+    """Generate Poisson-disc sampling pattern
+
+    Args:
+        img_shape (tuple of ints): length-2 image shape.
+        accel (float): Target acceleration factor. Greater than 1.
+        K (float): maximum number of samples to reject.
+        calib (tuple of ints): length-2 calibration shape.
+        dtype (Dtype): data type.
+        crop_corner (bool): Toggle whether to crop sampling corners.
+        return_density (bool): Toggle whether to return sampling density.
+        seed (int): Random seed.
+
+    Returns:
+        array: Poisson-disc sampling mask.
+
+    References:
+        Bridson, Robert. "Fast Poisson disk sampling in arbitrary dimensions."
+        SIGGRAPH sketches. 2007.
+
+    """
+    y, x = np.mgrid[:img_shape[-2], :img_shape[-1]]
+    x = np.maximum(abs(x - img_shape[-1] / 2) - calib[-1] / 2, 0)
+    x /= x.max()
+    y = np.maximum(abs(y - img_shape[-2] / 2) - calib[-2] / 2, 0)
+    y /= y.max()
+    r = np.sqrt(x ** 2 + y ** 2)
+
+    slope_max = 40
+    slope_min = 0
+    while slope_min < slope_max:
+        slope = (slope_max + slope_min) / 2.0
+        R = (1.0 + r * slope)
+        mask = _poisson(img_shape[-1], img_shape[-2], K, R, calib, seed)
+        if crop_corner:
+            mask *= r < 1
+
+        est_accel = img_shape[-1] * img_shape[-2] / np.sum(mask[:])
+
+        if abs(est_accel - accel) < 0.1:
+            break
+        if est_accel < accel:
+            slope_min = slope
+        else:
+            slope_max = slope
+
+    mask = mask.reshape(img_shape).astype(dtype)
+    if return_density:
+        return mask, r
+    else:
+        return mask
+
+
+@nb.jit(nopython=True, cache=True)  # pragma: no cover
+def _poisson(nx, ny, K, R, calib, seed):
+
+    mask = np.zeros((ny, nx))
+    f = ny / nx
+
+    if seed is not None:
+        rand_state = np.random.RandomState(int(seed))
+    else:
+        rand_state = np.random
+
+    pxs = np.empty(nx * ny, np.int32)
+    pys = np.empty(nx * ny, np.int32)
+    pxs[0] = rand_state.randint(0, nx)
+    pys[0] = rand_state.randint(0, ny)
+    m = 1
+    while (m > 0):
+
+        i = rand_state.randint(0, m)
+        px = pxs[i]
+        py = pys[i]
+        rad = R[py, px]
+
+        # Attempt to generate point
+        done = False
+        k = 0
+        while not done and k < K:
+
+            # Generate point randomly from R and 2R
+            rd = rad * (rand_state.random() * 3 + 1)**0.5
+            t = 2 * np.pi * rand_state.random()
+            qx = px + rd * np.cos(t)
+            qy = py + rd * f * np.sin(t)
+
+            # Reject if outside grid or close to other points
+            if qx >= 0 and qx < nx and qy >= 0 and qy < ny:
+
+                startx = max(int(qx - rad), 0)
+                endx = min(int(qx + rad + 1), nx)
+                starty = max(int(qy - rad * f), 0)
+                endy = min(int(qy + rad * f + 1), ny)
+
+                done = True
+                for x in range(startx, endx):
+                    for y in range(starty, endy):
+                        if (mask[y, x] == 1
+                            and (((qx - x) / R[y, x]) ** 2 +
+                                 ((qy - y) / (R[y, x] * f)) ** 2 < 1)):
+                            done = False
+                            break
+
+                    if not done:
+                        break
+
+            k += 1
+
+        # Add point if done else remove active
+        if done:
+            pxs[m] = qx
+            pys[m] = qy
+            mask[int(qy), int(qx)] = 1
+            m += 1
+        else:
+            pxs[i] = pxs[m - 1]
+            pys[i] = pys[m - 1]
+            m -= 1
+
+    # Add calibration region
+    mask[int(ny / 2 - calib[-2] / 2):int(ny / 2 + calib[-2] / 2),
+         int(nx / 2 - calib[-1] / 2):int(nx / 2 + calib[-1] / 2)] = 1
+
+    return mask
