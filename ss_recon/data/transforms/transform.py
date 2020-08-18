bug fix: broadcasting during renormalizing data; cpu transfer during evaluation

ss_recon/data/transforms/transform.py
@@ -1,11 +1,92 @@
 """Basic Transforms.
 """
 import torch
+from fvcore.common.registry import Registry
 
 from ss_recon.utils import complex_utils as cplx
 from ss_recon.utils import transforms as T
 
 from .subsample import build_mask_func
+
+
+NORMALIZER_REGISTRY = Registry("NORMALIZER")
+NORMALIZER_REGISTRY.__doc__ = """
+Registry for normalizing images
+"""
+
+def build_normalizer(cfg):
+    cfg = cfg.MODEL.NORMALIZER
+    name = cfg.NAME
+    obj = NORMALIZER_REGISTRY.get(name)(process_target=cfg.PROCESS_TARGET)
+    return obj
+
+
+class Normalizer():
+    def __init__(self, process_target=True):
+        self._process_target = process_target
+
+    def normalize(self, masked_kspace, image=None, target=None, mask=None):
+        pass
+
+    def undo(self, image, target=None, **kwargs):
+        pass
+
+
+@NORMALIZER_REGISTRY.register()
+class NoOpNormalizer(Normalizer):
+    def normalize(self, masked_kspace, image=None, target=None, mask=None):
+        return {
+            "masked_kspace": masked_kspace,
+            "target": target,
+            "mean": torch.tensor([0.0], dtype=torch.float32),
+            "std": torch.tensor([1.0], dtype=torch.float32),
+        }
+
+    def undo(self, image, target=None, **kwargs):
+        return {
+            "image": image,
+            "target": target
+        }
+
+
+@NORMALIZER_REGISTRY.register()
+class TopMagnitudeNormalizer(Normalizer):
+    def __init__(self, process_target = True, percentile=0.05):
+        super().__init__(process_target)
+        self._percentile = percentile
+
+    def normalize(self, masked_kspace, image, target=None, mask=None):
+        magnitude_vals = cplx.abs(image).reshape(-1)
+        k = int(round(self._percentile * magnitude_vals.numel()))
+        scale = torch.min(torch.topk(magnitude_vals, k).values)
+
+        masked_kspace /= scale
+        if self._process_target:
+            target /= scale
+        mean = torch.tensor([0.0], dtype=torch.float32)
+        std = scale.unsqueeze(-1)
+
+        return {
+            "masked_kspace": masked_kspace,
+            "target": target,
+            "mean": mean,
+            "std": std,
+        }
+
+    def undo(self, image, target=None, **kwargs):
+        mean = kwargs["mean"]
+        std = kwargs["std"]
+
+        mean = mean.view(mean.shape + (1,)*(image.ndim - mean.ndim)).to(image.device)
+        std = std.view(std.shape + (1,)*(image.ndim - std.ndim)).to(image.device)
+        image = image * std + mean
+        if target is not None and self._process_target:
+            target = target * std + mean
+
+        return {
+            "image": image,
+            "target": target,
+        }
 
 
 class Subsampler(object):
@@ -53,10 +134,17 @@ class DataTransform:
         # Build subsampler.
         # mask_func = build_mask_func(cfg)
         self._subsampler = Subsampler(self.mask_func)
+        self._normalizer = build_normalizer(cfg)
 
     def __call__(
-        self, kspace, maps, target, fname, slice, is_fixed,
-        acceleration: int = None
+        self, 
+        kspace, 
+        maps, 
+        target, 
+        fname, 
+        slice, 
+        is_fixed,
+        acceleration: int = None,
     ):
         """
         Args:
@@ -103,17 +191,16 @@ class DataTransform:
             kspace, mode="2D", seed=seed, acceleration=acceleration
         )
 
-        # Normalize data...
+        # Zero-filled Sense Recon.
         A = T.SenseModel(maps, weights=mask)
         image = A(masked_kspace, adjoint=True)
-        magnitude_vals = cplx.abs(image).reshape(-1)
-        k = int(round(0.05 * magnitude_vals.numel()))
-        scale = torch.min(torch.topk(magnitude_vals, k).values)
 
-        masked_kspace /= scale
-        target /= scale
-        mean = torch.tensor([0.0], dtype=torch.float32)
-        std = scale.unsqueeze(-1)
+        # Normalize
+        normalized = self._normalizer.normalize(masked_kspace, image, target, mask)
+        masked_kspace = normalized["masked_kspace"]
+        target = normalized["target"]
+        mean = normalized["mean"]
+        std = normalized["std"]
 
         # Get rid of batch dimension...
         masked_kspace = masked_kspace.squeeze(0)

ss_recon/evaluation/recon_evaluation.py
@@ -6,6 +6,7 @@ import numpy as np
 import torch
 from skimage.metrics import structural_similarity
 
+from ss_recon.data.transforms.transform import build_normalizer
 from ss_recon.utils import complex_utils as cplx
 
 from .evaluator import DatasetEvaluator
@@ -46,6 +47,7 @@ class ReconEvaluator(DatasetEvaluator):
 
         self._cpu_device = torch.device("cpu")
         self._logger = logging.getLogger(__name__)
+        self._normalizer = build_normalizer(cfg)
 
         # TODO: Uncomment when metadata is supported
         # self._metadata = MetadataCatalog.get(dataset_name)
@@ -84,22 +86,30 @@ class ReconEvaluator(DatasetEvaluator):
                 "instances" that contains :class:`Instances`.
         """
         N = outputs["pred"].shape[0]
-        preds = outputs["pred"].to(self._cpu_device)
-        targets = outputs["target"].to(self._cpu_device)
-        means = inputs["mean"].to(self._cpu_device)
-        stds = inputs["std"].to(self._cpu_device)
 
-        for i in range(N):
-            pred, target = preds[i], targets[i]
-            mean = means[i]
-            std = stds[i]
-            pred = pred * std + mean
-            target = target * std + mean
+        normalized = self._normalizer.undo(
+            outputs["pred"], outputs["target"], mean=inputs["mean"], std=inputs["std"]
+        )
+        preds = normalized["image"].to(self._cpu_device, non_blocking=True)
+        targets = normalized["target"].to(self._cpu_device, non_blocking=True)
 
-            # probably isn't best practice to hang onto each prediction.
-            prediction = {"pred": pred, "target": target}
+        self._predictions.extend([{"pred": preds[i], "target": targets[i]} for i in range(N)])
 
-            self._predictions.append(prediction)
+        # preds = outputs["pred"].to(self._cpu_device)
+        # targets = outputs["target"].to(self._cpu_device)
+        # means = inputs["mean"].to(self._cpu_device)
+        # stds = inputs["std"].to(self._cpu_device)
+        # for i in range(N):
+        #     pred, target = preds[i], targets[i]
+        #     mean = means[i]
+        #     std = stds[i]
+        #     pred = pred * std + mean
+        #     target = target * std + mean
+
+        #     # probably isn't best practice to hang onto each prediction.
+        #     prediction = {"pred": pred, "target": target}
+
+        #     self._predictions.append(prediction)
 
     def evaluate(self):
         if len(self._predictions) == 0:

ss_recon/modeling/loss_computer.py
@@ -3,6 +3,8 @@ from abc import ABC, abstractmethod
 import torch
 from fvcore.common.registry import Registry
 
+from ss_recon.data.transforms.transform import build_normalizer
+from ss_recon.evaluation.metrics import compute_nrmse
 from ss_recon.utils import complex_utils as cplx
 
 
@@ -15,6 +17,7 @@ and expected to return a LossComputer object.
 """
 
 EPS = 1e-11
+IMAGE_LOSSES = ["l1", "l2", "psnr", "nrmse"]
 
 
 def build_loss_computer(cfg, name):
@@ -22,6 +25,9 @@ def build_loss_computer(cfg, name):
 
 
 class LossComputer(ABC):
+    def __init__(self, cfg):
+        self._normalizer = build_normalizer(cfg)
+
     @abstractmethod
     def __call__(self, input, output):
         pass
@@ -36,8 +42,9 @@ class LossComputer(ABC):
         tgt_mag = cplx.abs(target).view(N, -1)
         l2 = torch.sqrt(torch.mean(abs_error ** 2, dim=1))
         psnr = 20 * torch.log10(tgt_mag.max(dim=1)[0] / (l2 + EPS))
+        nrmse = l2 / torch.sqrt(torch.mean(tgt_mag ** 2, dim=1))
 
-        metrics_dict = {"l1": l1, "l2": l2.mean(), "psnr": psnr.mean()}
+        metrics_dict = {"l1": l1, "l2": l2.mean(), "psnr": psnr.mean(), "nrmse": nrmse.mean()}
         loss = metrics_dict[loss_name]
         metrics_dict["loss"] = loss
 
@@ -47,8 +54,9 @@ class LossComputer(ABC):
 @LOSS_COMPUTER_REGISTRY.register()
 class BasicLossComputer(LossComputer):
     def __init__(self, cfg):
+        super().__init__(cfg)
         loss_name = cfg.MODEL.RECON_LOSS.NAME
-        assert loss_name in ["l1", "l2", "psnr"]
+        assert loss_name in IMAGE_LOSSES
         self.loss = loss_name
         self.renormalize_data = cfg.MODEL.RECON_LOSS.RENORMALIZE_DATA
 
@@ -57,10 +65,11 @@ class BasicLossComputer(LossComputer):
         target = output["target"].to(pred.device)
 
         if self.renormalize_data:
-            mean = input["mean"].to(pred.device)
-            std = input["std"].to(pred.device)
-            output = pred * std + mean
-            target = target * std + mean
+            normalized = self._normalizer.undo(
+                pred, target, mean=input["mean"], std=input["std"]
+            )
+            output = normalized["image"]
+            target = normalized["target"]
         else:
             output = pred
 
@@ -71,11 +80,12 @@ class BasicLossComputer(LossComputer):
 @LOSS_COMPUTER_REGISTRY.register()
 class N2RLossComputer(LossComputer):
     def __init__(self, cfg):
+        super().__init__(cfg)
         recon_loss = cfg.MODEL.RECON_LOSS.NAME
         consistency_loss = cfg.MODEL.CONSISTENCY.LOSS_NAME
 
-        assert recon_loss in ["l1", "l2", "psnr"]
-        assert consistency_loss in ["l1", "l2", "psnr"]
+        assert recon_loss in IMAGE_LOSSES
+        assert consistency_loss in IMAGE_LOSSES
 
         self.recon_loss = recon_loss
         self.consistency_loss = consistency_loss
@@ -96,10 +106,11 @@ class N2RLossComputer(LossComputer):
         pred: torch.Tensor = output["pred"]
         target = output["target"].to(pred.device)
         if self.renormalize_data:
-            mean = output["mean"].to(pred.device)
-            std = output["std"].to(pred.device)
-            output = pred * std + mean
-            target = target * std + mean
+            normalized = self._normalizer.undo(
+                pred, target, mean=input["mean"], std=input["std"]
+            )
+            output = normalized["image"]
+            target = normalized["target"]
         else:
             output = pred
 
@@ -150,4 +161,3 @@ class N2RLossComputer(LossComputer):
 
         metrics["loss"] = loss
         return metrics
-