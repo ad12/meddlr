34fcb2f936f63e66d1b5905cc22ca4acd33d9ea9
Arjun Desai <arjundd@stanford.edu>
2021-06-08 19:21:58 -0700

    refactor tests

--- tests/data/datasets/test_register_mrco.py
+++ tests/data/datasets/test_register_mrco.py
@@ -20,12 +20,5 @@ def test_load_metadata_from_csv():
             assert all("_metadata" in dd for dd in dataset_dicts)
 
 
-def test_filter_by_metadata():
-    dataset = "fastMRI_knee_multicoil_v0.0.1_train"
-    dataset_dicts = get_recon_dataset_dicts([dataset], filter_by=(("flip Angle_deg", 140),))
-    assert all(dd["_metadata"]["flip Angle_deg"] == 140 for dd in dataset_dicts)
-
-
-
 if __name__ == "__main__":
     unittest.main()
