75bd80ac00ef67ec0b4283ef5c00b069139a29a3
Arjun Desai <arjundd@stanford.edu>
2021-07-06 08:34:18 -0700

    add vif metric

--- ss_recon/evaluation/metrics.py
+++ ss_recon/evaluation/metrics.py
@@ -4,10 +4,20 @@ All metrics take a reference scan (ref) and an output/reconstructed scan (x).
 Both should be tensors with the last dimension equal to 2 (real/imaginary
 channels).
 """
+import numpy as np
+import scipy as scp
 import torch
 from skimage.metrics import structural_similarity
 
 from ss_recon.utils import complex_utils as cplx
+
+# Mapping from str to complex function name.
+_IM_TYPES_TO_FUNCS = {
+    "magnitude": cplx.abs,
+    "abs": cplx.abs,
+    "phase": cplx.angle,
+    "angle": cplx.angle,
+}
 
 
 def compute_mse(ref: torch.Tensor, x: torch.Tensor, is_batch=False, magnitude=False):
@@ -155,6 +165,94 @@ def compute_ssim(
         use_sample_covariance=use_sample_covariance,
         multichannel=multichannel,
     )
+
+
+def compute_vifp_mscale(
+    ref: torch.Tensor,
+    x: torch.Tensor,
+    sigma_nsq: float = 2.0,
+    eps: float = 1e-10,
+    im_type: str = None,
+):
+    """Compute visual information fidelity (VIF) metric.
+
+    This code is adapted from
+    https://github.com/aizvorski/video-quality/blob/master/vifp.py
+
+    Args:
+        ref (torch.Tensor): The reference image. This can be complex.
+        x (torch.Tensor): The target image. This can be complex.
+        sigma_nsq (float, optional): The visual noise parameter.
+            This may need to be fine-tuned over the dataset of interest.
+        eps (float, optional): The threshold below which data is considered to be 0.
+        im_type (str, optional): The image type to compute metric on.
+            Either ``'magnitude'`` (default) to compute metric on magnitude images
+            or ``'phase'`` to compute metric on phase/angle images.
+
+    Returns:
+        float: The metric value.
+
+    Note:
+        ``im_type`` is only valid if input is complex.
+    """
+    if cplx.is_complex(ref) or cplx.is_complex_as_real(ref):
+        ref = _IM_TYPES_TO_FUNCS[im_type](ref)
+    if cplx.is_complex(x) or cplx.is_complex_as_real(x):
+        x = _IM_TYPES_TO_FUNCS[im_type](x)
+
+    ref = np.squeeze(ref.numpy())
+    x = np.squeeze(x.numpy())
+
+    scale_val = 255.0 / ref.max()
+    ref *= scale_val
+    x *= scale_val
+
+    num = 0.0
+    den = 0.0
+    for scale in range(1, 5):
+
+        N = 2 ** (4 - scale + 1) + 1
+        sd = N / 5.0
+
+        if scale > 1:
+            sl = tuple(slice(None, None, 2) if dim > 1 else slice(None) for dim in ref.shape)
+            ref = scp.ndimage.gaussian_filter(ref, sd)
+            x = scp.ndimage.gaussian_filter(x, sd)
+            ref = ref[sl]
+            x = x[sl]
+
+        mu1 = scp.ndimage.gaussian_filter(ref, sd)
+        mu2 = scp.ndimage.gaussian_filter(x, sd)
+        mu1_sq = mu1 * mu1
+        mu2_sq = mu2 * mu2
+        mu1_mu2 = mu1 * mu2
+        sigma1_sq = scp.ndimage.gaussian_filter(ref * ref, sd) - mu1_sq
+        sigma2_sq = scp.ndimage.gaussian_filter(x * x, sd) - mu2_sq
+        sigma12 = scp.ndimage.gaussian_filter(ref * x, sd) - mu1_mu2
+
+        sigma1_sq[sigma1_sq < 0] = 0
+        sigma2_sq[sigma2_sq < 0] = 0
+
+        g = sigma12 / (sigma1_sq + eps)
+        sv_sq = sigma2_sq - g * sigma12
+
+        g[sigma1_sq < eps] = 0
+        sv_sq[sigma1_sq < eps] = sigma2_sq[sigma1_sq < eps]
+        sigma1_sq[sigma1_sq < eps] = 0
+
+        g[sigma2_sq < eps] = 0
+        sv_sq[sigma2_sq < eps] = 0
+
+        sv_sq[g < 0] = sigma2_sq[g < 0]
+        g[g < 0] = 0
+        sv_sq[sv_sq <= eps] = eps
+
+        num += np.sum(np.log10(1 + g * g * sigma1_sq / (sv_sq + sigma_nsq)))
+        den += np.sum(np.log10(1 + sigma1_sq / sigma_nsq))
+
+    vifp = num / den
+
+    return vifp
 
 
 compute_rmse = compute_l2
