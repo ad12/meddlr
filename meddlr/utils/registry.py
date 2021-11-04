c250bafca40437b9f7eb42d1ab83fee030d5a92c
Arjun Desai <arjundd@stanford.edu>
2021-11-03 20:20:15 -0700

    update registry __repr__ and alias management, delete meddlr/evaluation/metrics.py

--- meddlr/utils/registry.py
+++ meddlr/utils/registry.py
@@ -2,12 +2,17 @@ import functools
 from typing import Optional, Sequence
 
 from fvcore.common.registry import Registry as _Registry
+from tabulate import tabulate
 
 
 class Registry(_Registry):
     """Extension of fvcore's registry that supports aliases."""
 
     _ALIAS_KEYWORDS = ("_aliases", "_ALIASES")
+
+    def __init__(self, name: str):
+        super().__init__(name=name)
+        self._metadata_map = {}
 
     def _get_aliases(self, obj_func_or_class):
         for kw in self._ALIAS_KEYWORDS:
@@ -26,7 +31,7 @@ class Registry(_Registry):
                 if not isinstance(aliases, (list, tuple, set)):
                     aliases = [aliases]
                 for alias in aliases:
-                    self._do_register(alias, func_or_class)
+                    self._do_register(alias, func_or_class, is_alias=True)
                 return func_or_class
 
             kwargs = {"aliases": aliases}
@@ -40,7 +45,31 @@ class Registry(_Registry):
         if aliases is None:
             aliases = self._get_aliases(obj) if isinstance(obj, type) else []
         for alias in aliases:
-            self._do_register(alias, obj)
+            self._do_register(alias, obj, is_alias=True)
+
+    def _do_register(self, name: str, obj: object, **kwargs) -> None:
+        docstring = obj.__doc__
+        if docstring is None:
+            docstring = ""
+
+        aliases = self._get_aliases(obj) if isinstance(obj, type) else None
+        if not aliases:
+            aliases = None
+
+        self._metadata_map[name] = {
+            "name": name,
+            "description": kwargs.pop("description", docstring.split("\n")[0]),
+            "aliases": aliases,
+            **kwargs,
+        }
+        return super()._do_register(name, obj)
 
     def clear(self):
         self._obj_map = {}
+        self._metadata_map = {}
+
+    def __repr__(self) -> str:
+        metadata = [v for v in self._metadata_map.values() if not v.get("is_alias", False)]
+        metadata = sorted(metadata, key=lambda x: x["name"])
+        table = tabulate(metadata, headers="keys", tablefmt="fancy_grid")
+        return "Registry of {}:\n{}".format(self._name, table)
