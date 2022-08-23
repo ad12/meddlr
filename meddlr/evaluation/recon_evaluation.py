be7b022531a19160343409598135d9c5c8d5a8cb
Paul Michael Adamson <padamson@vigata.stanford.edu>
2022-08-23 14:28:08 -0700

    minor edit for how device is extracted in recon_evaluator for PR58

--- meddlr/evaluation/recon_evaluation.py
+++ meddlr/evaluation/recon_evaluation.py
@@ -97,7 +97,7 @@ class ReconEvaluator(ScanEvaluator):
         self._channel_names = channel_names
         self._structure_channel_by = structure_channel_by
         self._prefix = prefix
-        self._cfg = cfg
+        self.device = cfg.MODEL.DEVICE
 
         if save_scans and (not output_dir or not aggregate_scans):
             raise ValueError("`output_dir` and `aggregate_scans` must be specified to save scans.")
@@ -141,7 +141,6 @@ class ReconEvaluator(ScanEvaluator):
         self._predictions = []
         self._is_flushing = False
         self._memory = defaultdict(list)
-        device = self._cfg.MODEL.DEVICE
 
         metrics = self._metric_names
         prefix = self._prefix + "_" if self._prefix else ""
@@ -151,12 +150,12 @@ class ReconEvaluator(ScanEvaluator):
             slice_metrics,
             fmt=prefix + "{}",
             channel_names=self._channel_names,
-        ).to(device)
+        ).to(self.device)
         self.scan_metrics = build_metrics(
             scan_metrics,
             fmt=prefix + "{}_scan",
             channel_names=self._channel_names,
-        ).to(device)
+        ).to(self.device)
 
         self.slice_metrics.eval()
         self.scan_metrics.eval()
