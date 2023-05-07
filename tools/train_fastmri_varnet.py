eeed9df14de0a836c8b065b85940c5c8405a60d4
Arjun Desai <arjundd@stanford.edu>
2023-05-07 11:59:43 -0700

    add compatibility with fastmri varnet

--- tools/train_fastmri_varnet.py
+++ tools/train_fastmri_varnet.py
@@ -1,6 +1,7 @@
 """Train fastMRI with a varnet model."""
 from typing import Any, Dict
 
+import fastmri
 import torch
 import torchvision.utils as tv_utils
 from fastmri.models.varnet import VarNet
@@ -19,7 +20,7 @@ class VarNetWrapper(nn.Module):
         super().__init__()
         self.model = VarNet()
         self._device = None
-        self.vis_period = 100
+        self.vis_period = 20
 
     def visualize_training(self, mask: torch.Tensor, targets: torch.Tensor, preds: torch.Tensor):
         """Visualize kspace data and reconstructions.
@@ -40,16 +41,15 @@ class VarNetWrapper(nn.Module):
         storage = get_event_storage()
 
         with torch.no_grad():
-            mask = mask[0, ..., 0, :].unsqueeze(0).cpu()  # calc mask for first coil only
+            mask = mask[0, 0, ..., 0].unsqueeze(0).cpu()  # calc mask for first coil only
             targets = targets[0, ...].unsqueeze(0).cpu()
             preds = preds[0, ...].unsqueeze(0).cpu()
-
             all_images = torch.cat([preds, targets], dim=2)
 
             imgs_to_write = {
-                "images": cplx.abs(all_images),
-                "errors": cplx.abs(preds - targets),
-                "masks": mask,
+                "images": torch.abs(all_images),
+                "errors": torch.abs(preds - targets),
+                "masks": mask.type(torch.float32),
             }
 
             for name, data in imgs_to_write.items():
@@ -61,25 +61,46 @@ class VarNetWrapper(nn.Module):
         if self._device is None:
             self._device = next(self.model.parameters()).device
 
+        # shape: (batch, #coils, height, width, 2) for complex
+        kspace = torch.view_as_real(inputs["kspace"].permute(0, 3, 1, 2))
+        mask = inputs["mask"].permute(0, 3, 1, 2).type(torch.bool)
+        mask = torch.stack([mask, mask], dim=-1)
+        target = inputs["target"].to(self._device)
+
         inputs = dict(
-            masked_kspace=inputs["kspace"],
-            mask=inputs["mask"],
-            target=cplx.abs(inputs["target"]),
-            num_low_frequencies=0,
+            masked_kspace=kspace,
+            mask=mask,
+            num_low_frequencies=26,
         )
         inputs = move_to_device(inputs, self._device)
-        pred = self.model(**inputs)
-        output_dict = dict(pred=pred, target=inputs["target"])
+        pred = self.model(**inputs).unsqueeze(-1)
+        output_dict = dict(pred=pred, target=cplx.abs(target))
 
         if return_pp:
             output_dict.update({k: inputs[k] for k in ["mean", "std", "norm"]})
 
+        # breakpoint()
         if self.training and (vis_training or self.vis_period > 0):
             storage = get_event_storage()
             if vis_training or storage.iter % self.vis_period == 0:
-                self.visualize_training(inputs["mask"], inputs["target"], pred)
+                self.visualize_training(inputs["mask"], output_dict["target"], pred)
 
         return output_dict
+
+
+class Trainer(DefaultTrainer):
+    @classmethod
+    def build_loss_computer(cls, cfg):
+        loss = fastmri.SSIMLoss().to(cfg.MODEL.DEVICE)
+
+        def compute_loss(input, output):
+            pred = output["pred"].permute(0, 3, 1, 2)
+            target = output["target"].permute(0, 3, 1, 2)
+            data_range = target.reshape(target.shape[0], -1).max(dim=1).values
+            ssim_loss = loss(pred, target, data_range=data_range, reduced=True)
+            return {"loss": ssim_loss}
+
+        return compute_loss
 
 
 def main(args):
@@ -89,7 +110,7 @@ def main(args):
         raise NotImplementedError("Evaluation is not yet implemented")
 
     model = VarNetWrapper()
-    trainer = DefaultTrainer(cfg, model=model)
+    trainer = Trainer(cfg, model=model)
     trainer.resume_or_load(resume=args.resume, restart_iter=args.restart_iter)
     return trainer.train()
 
