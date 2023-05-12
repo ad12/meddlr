9842c0ccad70fd7d6f94b81cfb98b2e59760cd5a
Arjun Desai <ad12@users.noreply.github.com>
2023-05-12 09:29:34 -0700

    Config supports nested string formatting (#100)
    * add extra formatting stuff for config
    
    * add support for 1st-level nesting
    
    * add recursive set with index
    
    * add support for first level nesting

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -1,52 +1,106 @@
-import unittest
-
 import pytest
 
 from meddlr.config.config import get_cfg
 
 
-class TestConfig(unittest.TestCase):
-    def test_format_fields(self):
-        cfg = get_cfg()
-        cfg.DESCRIPTION.BRIEF = 'f"seed={SEED},project={DESCRIPTION.PROJECT_NAME}"'
-        cfg.format_fields()
-        assert cfg.DESCRIPTION.BRIEF == f"seed={cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}"
+def test_format_fields():
+    cfg = get_cfg()
+    cfg.DESCRIPTION.BRIEF = 'f"seed={SEED},project={DESCRIPTION.PROJECT_NAME}"'
+    cfg.format_fields()
+    assert cfg.DESCRIPTION.BRIEF == f"seed={cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}"
 
-        cfg = get_cfg()
-        cfg.DESCRIPTION.BRIEF = 'f"{SEED},project={DESCRIPTION.PROJECT_NAME}"'
-        cfg.format_fields()
-        assert cfg.DESCRIPTION.BRIEF == f"{cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}"
+    cfg = get_cfg()
+    cfg.DESCRIPTION.BRIEF = "f'{SEED},project={DESCRIPTION.PROJECT_NAME}'"
+    cfg.format_fields()
+    assert cfg.DESCRIPTION.BRIEF == f"{cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}"
 
-        cfg = get_cfg()
-        cfg.DESCRIPTION.BRIEF = 'f"{SEED},project={DESCRIPTION.PROJECT_NAME}-today"'
-        cfg.format_fields()
-        assert cfg.DESCRIPTION.BRIEF == f"{cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}-today"
+    cfg = get_cfg()
+    cfg.DESCRIPTION.BRIEF = 'f"{SEED},project={DESCRIPTION.PROJECT_NAME}-today"'
+    cfg.format_fields()
+    assert cfg.DESCRIPTION.BRIEF == f"{cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}-today"
 
-        cfg = get_cfg()
-        cfg.AUG_TRAIN.UNDERSAMPLE.ACCELERATIONS = (6, 8)
-        cfg.DESCRIPTION.BRIEF = (
-            'f"{AUG_TRAIN.UNDERSAMPLE.ACCELERATIONS},project={DESCRIPTION.PROJECT_NAME}-today"'
-        )
-        cfg.format_fields(unroll=True)
-        expected = "{},project={}-today".format(
-            "-".join(str(x) for x in cfg.AUG_TRAIN.UNDERSAMPLE.ACCELERATIONS),
-            cfg.DESCRIPTION.PROJECT_NAME,
-        )
-        assert cfg.DESCRIPTION.BRIEF == expected
 
-    def test_get_recursive(self):
-        cfg = get_cfg()
-        cfg.DESCRIPTION.BRIEF = "foobar"
+def test_format_fields_unroll():
+    cfg = get_cfg()
+    cfg.AUG_TRAIN.UNDERSAMPLE.ACCELERATIONS = (6, 8)
+    cfg.DESCRIPTION.BRIEF = (
+        'f"{AUG_TRAIN.UNDERSAMPLE.ACCELERATIONS},project={DESCRIPTION.PROJECT_NAME}-today"'
+    )
+    cfg.format_fields(unroll=True)
+    expected = "{},project={}-today".format(
+        "-".join(str(x) for x in cfg.AUG_TRAIN.UNDERSAMPLE.ACCELERATIONS),
+        cfg.DESCRIPTION.PROJECT_NAME,
+    )
+    assert cfg.DESCRIPTION.BRIEF == expected
 
-        assert cfg.get_recursive("DESCRIPTION.BRIEF") == "foobar"
-        assert cfg.get_recursive("DESCRIPTION.FOO", None) is None
-        with pytest.raises(KeyError):
-            cfg.get_recursive("DESCRIPTION.FOO")
 
-    def test_get_recursive_with_list(self):
-        cfg = get_cfg()
-        cfg.DESCRIPTION.BRIEF = "foobar"
+def test_format_fields_nested():
+    """Format fields inside lists and tuples."""
+    cfg = get_cfg()
+    cfg.DESCRIPTION.TAGS = ['f"seed={SEED},project={DESCRIPTION.PROJECT_NAME}"', "foobar"]
+    cfg.format_fields()
+    assert cfg.DESCRIPTION.TAGS == [
+        f"seed={cfg.SEED},project={cfg.DESCRIPTION.PROJECT_NAME}",
+        "foobar",
+    ]
 
-        assert cfg.get_recursive("DESCRIPTION.BRIEF[0]") == "f"
-        with pytest.raises(IndexError):
-            cfg.get_recursive("DESCRIPTION.BRIEF[1000]")
+
+def test_get_recursive():
+    cfg = get_cfg()
+    cfg.DESCRIPTION.BRIEF = "foobar"
+
+    assert cfg.get_recursive("DESCRIPTION.BRIEF") == "foobar"
+    assert cfg.get_recursive("DESCRIPTION.FOO", None) is None
+    with pytest.raises(KeyError):
+        cfg.get_recursive("DESCRIPTION.FOO")
+
+
+def test_get_recursive_with_index():
+    cfg = get_cfg()
+    # Index into string.
+    cfg.DESCRIPTION.BRIEF = "foobar"
+    assert cfg.get_recursive("DESCRIPTION.BRIEF[0]") == "f"
+    with pytest.raises(IndexError):
+        cfg.get_recursive("DESCRIPTION.BRIEF[1000]")
+
+    # Index into list/tuple.
+    cfg.DESCRIPTION.TAGS = ["foobar", "baz"]
+    assert cfg.get_recursive("DESCRIPTION.TAGS[0]") == "foobar"
+    assert cfg.get_recursive("DESCRIPTION.TAGS[1]") == "baz"
+    assert cfg.get_recursive("DESCRIPTION.TAGS[-1]") == "baz"
+    with pytest.raises(IndexError):
+        cfg.get_recursive("DESCRIPTION.TAGS[1000]")
+
+    # Multi-nested index.
+    # cfg.DESCRIPTION.TAGS = [("foobar", "baz"), "goodbye"]
+    # assert cfg.get_recursive("DESCRIPTION.TAGS[0][0]") == "foobar"
+
+
+def test_set_recursive():
+    cfg = get_cfg()
+    cfg.set_recursive("DESCRIPTION.BRIEF", "foobar")
+    assert cfg.DESCRIPTION.BRIEF == "foobar"
+
+
+def test_set_recursive_with_index():
+    cfg = get_cfg()
+    cfg.DESCRIPTION.TAGS = ("foobar", "baz")
+    cfg.set_recursive("DESCRIPTION.TAGS[0]", "hello")
+    assert cfg.DESCRIPTION.TAGS == ("hello", "baz")
+
+    # Nested list replace with different type.
+    cfg = get_cfg()
+    cfg.DESCRIPTION.TAGS = [("foobar", "baz"), "goodbye"]
+    cfg.set_recursive("DESCRIPTION.TAGS[0]", "hello")
+    assert cfg.DESCRIPTION.TAGS == ["hello", "goodbye"]
+
+    # Cannot replace values in a string.
+    cfg = get_cfg()
+    with pytest.raises(TypeError):
+        cfg.set_recursive("DESCRIPTION.BRIEF[0]", "hello")
+
+    # Multi-nested list.
+    # cfg = get_cfg()
+    # cfg.DESCRIPTION.TAGS = [("foobar", "baz"), "goodbye"]
+    # cfg.set_recursive("DESCRIPTION.TAGS[0][0]", "hello")
+    # assert cfg.DESCRIPTION.TAGS == [("hello", "baz"), "goodbye"]
